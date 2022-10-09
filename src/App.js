9c95403ac0a0166c312facd88d1977e1665f1f00
Franklyn Viana <franklyn_vs_@hotmail.com>
2022-10-09 00:16:28 -0300

    buscador de cep finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react'
+import './styles.css'
+import api from './services/api.js'
 
-function App() {
+export function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+  async function handleSearch() {
+    if (input === '') {
+      alert('Preencha algum CEP!')
+      return
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    } catch {
+      alert('CEP não encontrado')
+      setInput('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador: CEP</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#ffffff" />
+        </button>
+      </div>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
